a813610b6711025a6bf9eca237d18caa11cc77f9
Ruben Renart <rubenrenart@gmail.com>
2025-01-04 17:17:37 +0100

    v0.9 - Vídeo Terminado. No se ha visto aún el resultado en móvil.

--- app/src/main/java/com/example/pdm_logininstagram/LoginScreen.kt
+++ app/src/main/java/com/example/pdm_logininstagram/LoginScreen.kt
@@ -31,17 +31,14 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.node.ModifierNodeElement
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -275,18 +272,51 @@ fun LoginSocial() {
 @Composable
 fun Footer(modifier: Modifier)
 {
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ){
+        HorizontalDivider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFF9F9F9F))
+                .height(1.dp)
+        )
 
+        Spacer(modifier = Modifier.size(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = "Don't have an account?",
+                fontSize = 12.sp,
+                color = Color(0xFFB5B5B5)
+            )
+            Text(
+                text = "Sign up",
+                fontSize = 12.sp,
+                color = Color(0xFF4EA8E9),
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.clickable { createAccount() }
+            )
+        }
+    }
 }
 
-
 fun checkForgotPassword() {
-    TODO("Not yet implemented")
+    //TODO("Not yet implemented")
 }
 
 fun checkLogin() {
-    TODO("Not yet implemented")
+    //TODO("Not yet implemented")
 }
 
 fun checkSocial() {
-    TODO("Not yet implemented")
+    //TODO("Not yet implemented")
+}
+
+fun createAccount() {
+    //TODO("Not yet implemented")
 }
